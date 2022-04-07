5cc6b21670eff3b5beaf6c0100115b5e8d4c6682
Alex <azhosford@gmail.com>
2022-04-06 23:05:06 -0700

    Did Chris's bullshit with all the math operations and stuff, added a file to calculate GCD it broke things I think they work now. good to go :)

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -18,4 +18,67 @@ class RecipeModel: ObservableObject {
         
         
     }
+    
+    static func getPortion(ingredient: Ingredient, recipeServings: Int, targetServings: Int) -> String {
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
+        
+        if ingredient.num != nil {
+            //Get a single serving size by multiplying denominator by the recipie servings
+            denominator *= recipeServings
+            
+            //Get target portion by multiplying numerator by target servings
+            numerator *= targetServings
+            
+            //Reduce fraction by GCD
+            let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divisor
+            denominator /= divisor
+            
+            //Get the whole portions if num is greater than denom
+            if numerator >= denominator {
+                //Calculated whole portions
+                wholePortions = numerator / denominator
+                
+                //Calculate reaminder
+                numerator = numerator % denominator
+                
+                portion += String(wholePortions)
+            }
+            
+            //Express remainder as fraction
+            if numerator > 0 {
+                
+                //assign remainder
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+                
+            }
+        }
+        if var unit = ingredient.unit {
+            
+            //If we need to pluralize
+            if wholePortions > 1 {
+                //claculate appropriate suffix
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                } else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                } else {
+                    unit += "s"
+                }
+                
+            }
+            
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "
+            
+            return portion + unit
+        }
+        
+        return portion
+    }
 }
